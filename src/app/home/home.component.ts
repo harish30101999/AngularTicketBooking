9bf9bbd23cac1793b8d5c3115e651670bdaec29c
harish30101999 <109505934+harish30101999@users.noreply.github.com>
2022-11-14 16:52:29 +0530

    6 files modified

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,6 +6,7 @@ import { AccountService } from '../_services';
 @Component({ templateUrl: 'home.component.html' })
 export class HomeComponent {
     user: User;
+    myImage:string="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVtDW6C65Xb6_W-ot54cP-h1bz8vh3K7QuzQ&usqp=CAU"
 
     constructor(private accountService: AccountService) {
         this.user = this.accountService.userValue;
